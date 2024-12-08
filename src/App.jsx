f366852eb3bc44389b856f26312ca9e74636b999
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-12-08 15:03:14 +0530

    navbar
    created responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-full overflow-hidden">
+      <Header />
     </div>
   );
 };
